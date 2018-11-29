b023aeeeeb55b08cacf7bf530f3050022a73ebf9
jleetaylor2020 <jleetaylor2020@icloud.com>
2018-11-29 16:15:51 -0600

    updates

---
+++ scripts2.js
@@ -0,0 +1,24 @@
+// // creating a javascript variable
+// let myButton = document.getElementById('best-btn');
+
+// // creating a jQuery variable
+// let $myButton = $('best-btn');
+// $(document).ready(function () {
+$('input#btnSubmit').click(function () {
+    $('<p>I am a message when button is clicked!</p>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
